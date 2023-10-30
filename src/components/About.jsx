d201bf0ec48d6c889b4246d6ea2bdf1b3d689621
Felipe Lyra <fgmlyra@gmail.com>
2023-10-29 19:04:26 -0500

    Added the info for the paragraphs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
         <div id='about' className=' max-w-[1040px] m-auto md:pl-20 p-4 py-16 bg-slate-50'>
             <h1 className=' text-4xl font-bold text-center text-slate-900'>About</h1>
             <h2 className=' pt-11 text-2xl'>
-                I am someone who's deeply passionate about coding and the ever-evolving world of Coding. My journey in this field is driven by an insatiable love for solving complex problems and creating innovative solutions through code. I embrace the challenges that come with the territory, as they fuel my desire to learn and adapt.
+                I am someone who's deeply passionate about coding and the ever-evolving world of Coding. My journey in this field started when I completer the Rice University Coding BootCamp, and because of it is driven by an insatiable love for solving complex problems and creating innovative solutions through code. I embrace the challenges that come with the territory, as they fuel my desire to learn and adapt.
             </h2>
             <h2 className=' pt-11 text-2xl'>
                 I also have a deep appreciation for collaboration and effective communication. During my time at The Walt Disney World, I honed my communication skills, particularly in assisting guests when they encountered inconveniences or unexpected issues, swiftly turning their experiences magical within just a brief interaction. This experience ignited a passion in me for developing software solutions that can enhance the quality of people's lives.
